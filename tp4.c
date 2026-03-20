05b327e39cdaa85a219cefb10155a3d35ea6f308
Keven Moreno Araújo <kevenmorenoaraujo@gmail.com>
2026-03-20 22:23:27 +0000

    inicio

---
+++ tp4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdio.h>
+void inverte(char s[]){
+    int tam;
+    while(s[tam]!='\0'){
+        tam++;
+    }
+    char espelho[100];
+    for (int i=0;i<tam;i++){
+        espelho[i]=s[tam-1-i];
+    }
+    print("%s \n",espelho);
+}
+
+int main(){
+    char s[100], ;
+    while(scanf("%s",s)!='EOF'){
+        invrter(s);
+    }
+    return 0;
+    
+}
